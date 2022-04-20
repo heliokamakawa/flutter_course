9968b93e4767c3f06fb88e17c8fc7f57d29913f6
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2022-04-20 08:48:30 -0300

    Update c_anonymous_functions.dart

--- module4/b_anonymous_functions/lib/c_anonymous_functions.dart
+++ module4/b_anonymous_functions/lib/c_anonymous_functions.dart
@@ -18,9 +18,8 @@ void show() {
   var resultado = '';
 
   if(escolha == 1){
-    resultado = interfaceNotas(n1, n2, (){
+    resultado = verificarAprovacao(n1, n2, (){
       double media = (n1 + n2) / 2;
-
       if(media > 6){
         return 'aprovado';
       }else{
@@ -28,46 +27,52 @@ void show() {
       }
     });
   }else if(escolha == 2){
-    resultado = interfaceNotas(n1, n2, (){
-      double media = (n1 + n2) / 2;
-      if(n1 == n2){
-        return n1;
-      } else if(n1 > n2){
-        return n1;
+    resultado = verificarAprovacao(n1, n2, (){
+      double maior = nota1;
+      if(nota2 > nota1){
+        maior = nota2;
+      } 
+      if(media >= 6){
+        return 'aprovado';
       }else{
-        return 'a maior nota é $n2';
+        return 'reprovado';
       }
-    }
-    );
+    });
   } else {
-    resultado = interfaceNotas(n1, n2, () {
-      return 'A média é ${(n1 + n2) / 2}';
-    }
-    );
+    resultado = verificarAprovacao(n1, n2, () {
+      double menor = nota1;
+      if(nota2 < nota1){
+        maior = nota2;
+      } 
+      if(media >= 6){
+        return 'aprovado';
+      }else{
+        return 'reprovado';
+      }
+    });
   }
   print(resultado);
 }
 
-String interfaceNotas(double nota1, double nota2, Function acao){
+String verificarAprovacao(double nota1, double nota2, Function acao){
     return acao(nota1,nota2);
 }
 
 /*
-Atividade I
-→ interfaceSalario 
-→ bonificar - receber salario e o valor do bônus. Realizar o cálculo e informar se o total é maior que o salário mínimo.
-→ descontar - receber salario e o valor do desconto. Realizar o cálculo e informar se o total é positivo.
-→ comparar - receber dois salario. Retornar o maior salário.
-*/
+Atividade I 
+1). Qual a diferença em relação ao código anterior? Na sua opinão, o que melhorou e o que piorou?
+2). O que é e para que serve parâmtros do tipo função? Em que caso utilizamos?
 
-/*
-Atividade II
+Atividade II - Defina 3 funções do mesmo contexto e uma interface para estas funções
+→ Interface Salario 
+→ Receber salario e um valor. Bonificar o valor no salário e informar se o total é maior que o salário mínimo.
+→ Receber salario e um valor. Descontar o valor no salário e informar se ainda resta salário a receber.
+→ Receber salario e um valor. Calcular e informar o % que o valor da comissão representa em relação ao salário.
+
+Atividade III
 Pensar em um projeto único 
 Fazer um exemplo seu
-Fazer comentários de tudo que entendeu
 */
-
-
 
 
 
