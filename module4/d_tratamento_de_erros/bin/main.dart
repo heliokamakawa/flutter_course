f1cfba0a7ce806289aae6a006352e14e3ae95a17
heliokamakawa <heliokamakawa@gmail.com>
2022-05-04 09:27:08 -0300

    tratamento de erros

---
+++ module4/d_tratamento_de_erros/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:d_tratamento_de_erros/a_atividade.txt' as d_tratamento_de_erros;
+
+void main(List<String> arguments) {
+  
+}
---
+++ module4/d_tratamento_de_erros/test/d_tratamento_de_erros_test.dart
@@ -0,0 +1,7 @@
+import 'package:d_tratamento_de_erros/a_atividade.txt';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+  });
+}
