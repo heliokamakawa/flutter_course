59eb630aafda207e1e696c0d323746f4ba71e70e
heliokamakawa <heliokamakawa@gmail.com>
2022-05-28 07:58:38 -0300

    variáveis

---
+++ module 2/variaveis/lib/07_atividade01.dart
@@ -0,0 +1,11 @@
+/*
+01) O que são variáveis? Qual é a sintaxe da declaração? Escreva o código de exemplo.
+02) Quais são os tipos de variáveis existentes em Dart? Explique a diferença entre os tipos. 
+03) O que é interpolação? Qual a sua função? Escreva o código de exemplo.
+04) Qual a diferença entre a declaração com e sem atribuição?
+05) Quais são os cuidados que devemos ter para definir o nome de uma variável?
+06) Quais são as regras de sintaxe para declarar uma variável?
+07) Qual padrão devemos respeitar para definir nomes de variáveis? Escreva o código de exemplo.
+08) O que é e para que serve a inferência de tipos? Escreva o código de exemplo.
+09) Na declaração de uma variável, qual a diferença entre a definição explícita de tipos e a inferência de tipos? Em que contexto deve-se utilizar cada uma? Escreva o código de exemplo.
+*/
