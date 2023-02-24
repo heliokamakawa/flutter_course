0f7dc93939add1fa19e70acf437356fd16c022e5
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2023-02-24 15:34:17 -0300

    Update 07_atividade01.dart

--- lib/modulo 02 - variaveis/07_atividade01.dart
+++ lib/modulo 02 - variaveis/07_atividade01.dart
@@ -10,4 +10,8 @@
 09) Qual a diferença entre a definição explícita e a inferência de tipos? Em que contexto deve-se utilizar cada uma? Escreva o código de exemplo.
 10) Pode-se afirmar que, a declaração com a inferência de tipos gera-se uma variável dinâmica? Justifique.
 11) O que são variáveis dinâmicas? Quais cuidados devemos ter?
+12) Em dart, pode-se definir constantes de quais maneiras? Explique as diferenças e quando devemos utilizar cada uma.
+13) Solicite 3 números e informe: (1) a média de todos os números; (2) o maior valor; e (4) a soma de todos os números.
+14) Pense em um pequeno algoritmo que solicite dados de um usuário (todas do tipo String), de modo que, seja necessário realizar todas as conversões de dados.
+15) Escreve um código que exemplifique a diferença semântica entre const e final.
 */
