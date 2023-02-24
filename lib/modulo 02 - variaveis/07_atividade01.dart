b46fc26f5b97e995c40525473a690f83f0517973
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2023-02-24 15:28:28 -0300

    Update 07_atividade01.dart

--- lib/modulo 02 - variaveis/07_atividade01.dart
+++ lib/modulo 02 - variaveis/07_atividade01.dart
@@ -7,5 +7,7 @@
 06) Quais são as regras de sintaxe para declarar uma variável?
 07) Qual padrão devemos respeitar para definir nomes de variáveis? Escreva o código de exemplo.
 08) O que é e para que serve a inferência de tipos? Escreva o código de exemplo.
-09) Na declaração de uma variável, qual a diferença entre a definição explícita de tipos e a inferência de tipos? Em que contexto deve-se utilizar cada uma? Escreva o código de exemplo.
+09) Qual a diferença entre a definição explícita e a inferência de tipos? Em que contexto deve-se utilizar cada uma? Escreva o código de exemplo.
+10) Pode-se afirmar que, a declaração com a inferência de tipos gera-se uma variável dinâmica? Justifique.
+11) O que são variáveis dinâmicas? Quais cuidados devemos ter?
 */
